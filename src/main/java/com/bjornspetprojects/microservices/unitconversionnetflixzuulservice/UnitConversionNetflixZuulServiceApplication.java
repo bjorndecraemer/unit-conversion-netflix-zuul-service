77dc29cb91c6542979b6acd264b636acee6b650e
Bjorn De Craemer <bjorn.decraemer@yahoo.com>
2019-01-29 01:25:15 +0100

    Implement Netflix Zuul Service

--- src/main/java/com/bjornspetprojects/microservices/unitconversionnetflixzuulservice/UnitConversionNetflixZuulServiceApplication.java
+++ src/main/java/com/bjornspetprojects/microservices/unitconversionnetflixzuulservice/UnitConversionNetflixZuulServiceApplication.java
@@ -2,8 +2,12 @@ package com.bjornspetprojects.microservices.unitconversionnetflixzuulservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
 public class UnitConversionNetflixZuulServiceApplication {
 
 	public static void main(String[] args) {
