5ea001b73e9180d3863da298063b899f357f467a
Bjorn De Craemer <bjorn.decraemer@yahoo.com>
2019-01-29 01:06:16 +0100

    Initial commit

---
+++ src/main/java/com/bjornspetprojects/microservices/unitconversionnetflixzuulservice/UnitConversionNetflixZuulServiceApplication.java
@@ -0,0 +1,14 @@
+package com.bjornspetprojects.microservices.unitconversionnetflixzuulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitConversionNetflixZuulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitConversionNetflixZuulServiceApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/bjornspetprojects/microservices/unitconversionnetflixzuulservice/UnitConversionNetflixZuulServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.bjornspetprojects.microservices.unitconversionnetflixzuulservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UnitConversionNetflixZuulServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
